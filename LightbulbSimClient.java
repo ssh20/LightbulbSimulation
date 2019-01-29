689dc7b6cbf64ab86439cf11aa6fa9088d1af422
Suhas Hoysala <ssh20@pitt.edu>
2019-01-29 09:09:32 -0500

    Ignore that last one, working for now, but inefficient

---
+++ LightbulbSimClient.java
@@ -0,0 +1,11 @@
+package LightbulbSimulation;
+
+public class LightbulbSimClient {
+
+	public static void main(String[] args) {
+		LightbulbSimulator.setStates(3);
+		LightbulbSimulator.solveAll(7);
+
+	}
+
+}
--- LightbulbSimulator.java
+++ LightbulbSimulator.java
@@ -1,85 +1,133 @@
 package LightbulbSimulation;
 
+import java.util.HashMap;
 import java.util.HashSet;
-import java.util.LinkedHashSet;
 
 public class LightbulbSimulator {
-	private int lightbulbs;
-	private int states;
-	LinkedHashSet<Integer> steps = new LinkedHashSet<Integer>();
-	HashSet<Integer> configs = new HashSet<Integer>();
-	HashSet<Integer> solvableConfigs = new HashSet<Integer>();
-	HashSet<Integer> unsolvableConfigs = new HashSet<Integer>();
+	private static int lightbulbs;
+	private static int states = 2;
+	private static HashMap<Integer,Integer> steps = new HashMap<Integer,Integer>();
+	private static HashSet<String> configs = new HashSet<String>();
+	private static HashSet<String> solvableConfigs = new HashSet<String>();
+	private static HashSet<String> unsolvableConfigs = new HashSet<String>();
 	
-	public LightbulbSimulator(int lightbulbs) {
-		this.lightbulbs = lightbulbs;
-		this.states = 2;
-	}
-	
-	public int getStates() {
+	public static int getStates() {
 		return states;
 	}
 	
-	public void setStates(int states) {
-		this.states = states;
+	public static void setStates(int states1) {
+		states = states1;
 	}
 	
-	public int getLightbulbs() {
+	public static int getLightbulbs() {
 		return lightbulbs;
 	}
 	
-	public void setLightbulbs(int lightbulbs) {
-		this.lightbulbs = lightbulbs;
+	public static void setLightbulbs(int lightbulbs1) {
+		lightbulbs = lightbulbs1;
 	}
 	
-	public void solveAll() {
+	public static void solveAll(int lightbulbs) {
 		for(int i = 0; i < Math.pow(states, lightbulbs); i++) {
-			
+			String config = Integer.toString(i,states);
+			config = padString(config);
+			String canOrCannot = "";
+			if(solve(config)) {
+				canOrCannot = "can";
+				solvableConfigs.addAll(configs);
+			}
+			else {
+				canOrCannot = "cannot";
+				unsolvableConfigs.addAll(configs);
+			}
+			System.out.println("For "+config+", we "+canOrCannot+" solve this configuration.");
+			if(canOrCannot.equals("can")) {
+				System.out.print("Steps taken to solve system: {");
+				for(Integer step: steps.keySet()) {
+					for(int j = 0; j < steps.get(step); j++) {
+						System.out.print(step);
+						System.out.print(", ");
+					}
+				}
+				System.out.print("}\n");
+			}
+			steps.clear();
+			configs.clear();
 		}
 	}
 	
-	public boolean solve(int config) {
-		if(config==0)
+	public static boolean solve(String config) {
+		config = padString(config);
+		if(Integer.parseInt(config, states)==0)
 			return true;
+		
 		boolean removeElement = false;
-		int[] bestGuess = bestGuess(config);
-		if(bestGuess!=null&&configs.add(config))
-			for(int j = 0; j < lightbulbs; j++) {
-				if(bestGuess[j]!=0)
-					if(steps.add(bestGuess[j])) {
-						removeElement = true;
-						int newConfig = switchBulb(config, bestGuess[j]);
-						if(solve(newConfig))
-							return true;
-					}
+			for(int j = 1; j < lightbulbs+1; j++) {
+				if(!steps.containsKey(j)) {
+					steps.put(j,1);
+					removeElement = true;
+					String newConfig = switchBulb(config, j);
+					if(solve(newConfig))
+						return true;
+				}
+				if(steps.get(j)<states-1&&!removeElement) {
+					int times = steps.get(j);
+					steps.remove(j);
+					steps.put(j, times+1);
+					removeElement = true;
+					String newConfig = switchBulb(config, j);
+					if(solve(newConfig))
+						return true;
+				}
 				if(!steps.isEmpty()&&removeElement) {
-					steps.remove(bestGuess[j]);
+					int times = steps.get(j);
+					steps.remove(j);
+					if(times-1>0)
+						steps.put(j,times-1);
+						
 					removeElement = false;
 				}
 			}
 		return false;
 	}	
 	
-	public int switchBulb(int config, int theSwitch) {
-		int switchConfig = 7;
-		int mask = switchConfig;
-		if(theSwitch ==lightbulbs)
-			mask = switchConfig >> 1;
-		else
-			mask = switchConfig << (lightbulbs-theSwitch-1);
-		int mask2 = (1 << lightbulbs) - 1;
-		return (config^mask)&mask2;
+	public static String switchBulb(String config, int theSwitch) {
+		StringBuilder newConfig = new StringBuilder(config);
+		int bitAt,bitNext,bitPrev;
+		
+		if(theSwitch==1) {
+			bitAt = (Character.getNumericValue(config.charAt(0))+1)%states;
+			bitNext = (Character.getNumericValue(config.charAt(1))+1)%states;
+			newConfig.setCharAt(theSwitch-1, (char)(bitAt+48));
+			newConfig.setCharAt(theSwitch, (char)(bitNext+48));
+		}
+		else if(theSwitch ==lightbulbs) {
+			bitAt = (Character.getNumericValue(config.charAt(lightbulbs-1))+1)%states;
+			bitPrev = (Character.getNumericValue(config.charAt(lightbulbs-2))+1)%states;
+			newConfig.setCharAt(theSwitch-1, (char)(bitAt+48));
+			newConfig.setCharAt(theSwitch-2, (char)(bitPrev+48));
+		}
+		else {
+			bitAt = (Character.getNumericValue(config.charAt(theSwitch-1))+1)%states;
+			bitPrev = (Character.getNumericValue(config.charAt(theSwitch-2))+1)%states;
+			bitNext = (Character.getNumericValue(config.charAt(theSwitch))+1)%states;
+			newConfig.setCharAt(theSwitch-1, (char)(bitAt+48));
+			newConfig.setCharAt(theSwitch-2, (char)(bitPrev+48));
+			newConfig.setCharAt(theSwitch, (char)(bitNext+48));
+		}
+		return newConfig.toString();
 	}
 	
-	public int[] bestGuess(int config) {
+	public int[] bestGuess(String config) {
 		int[] bestGuess = new int[lightbulbs];
 		int[][] pairWise = new int[lightbulbs][2];
 		for(int i = 1; i < lightbulbs+1; i++) {
-			if(steps.add(i)) {
+			if(steps.containsKey(i)) {
 				steps.remove(i);
-				int atBit = getBit(config,lightbulbs-i);
-				int leftBit = getBit(config,lightbulbs-i+1);
-				int rightBit = getBit(config,lightbulbs - i-1);
+				//int atBit = getBit(config,lightbulbs-i);
+				int atBit = Character.getNumericValue(config.charAt(lightbulbs-i));
+				int leftBit = Character.getNumericValue(config.charAt(lightbulbs-i+1));
+				int rightBit = Character.getNumericValue(config.charAt(lightbulbs-i-1));
 				if(i!=1&&leftBit==0)
 					leftBit = -1;
 				if(i!=lightbulbs&&rightBit==0)
@@ -117,8 +165,8 @@ public class LightbulbSimulator {
 	   return (config >> position) & 1;
 	}
 	
-	public String intToString(int number) {
-	    String result = Integer.toString(number, 2);
+	private static String padString(String config) {
+	    String result = config;
 	    while(result.length()<lightbulbs)
 	    	result = "0"+result;
 	    return result;
